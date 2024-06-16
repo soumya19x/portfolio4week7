dc919a161fdb37399c1877bfe2499531fc289683
soumya19xRMIT <s4023903@student.rmit.edu.au>
2024-06-16 19:16:25 +1000

    feat: Integrate Thymeleaf templates and update Java controllers
    - Improved App.java to set up Javalin server with Thymeleaf integration and configure routes.
    - Added JDBCConnection.java to manage SQLite database connections and queries.
    - Created Movie.java to represent a movie entity.
    - Updated PageIndex.java to handle the index page rendering using Thymeleaf.
    - Updated PageMoviesList.java to handle the movies list page rendering using Thymeleaf.
    - Updated PageMoviesType.java to handle the movies by type page rendering using Thymeleaf.
    - Modified index.html to use Thymeleaf for dynamic content.
    - Modified movies.html to display the list of movies dynamically using Thymeleaf.
    - Modified moviestype.html to handle form submissions and display movies by type dynamically using Thymeleaf.
    
    Changes:
    - App.java: Set up Javalin server, configure routes, integrate Thymeleaf.
    - JDBCConnection.java: Manage SQLite database connections and queries.
    - Movie.java: Represent a movie entity.
    - PageIndex.java: Handle index page rendering with Thymeleaf.
    - PageMoviesList.java: Handle movies list page rendering with Thymeleaf.
    - PageMoviesType.java: Handle movies by type page rendering with Thymeleaf.
    - index.html: Use Thymeleaf for dynamic content.
    - movies.html: Display list of movies dynamically with Thymeleaf.
    - moviestype.html: Handle form submissions, display movies by type dynamically with Thymeleaf.
    
    This commit integrates Thymeleaf for rendering dynamic content in our web application, improves the structure of our Java code, and ensures better maintainability and scalability of the application.

--- src/main/java/app/PageMoviesType.java
+++ src/main/java/app/PageMoviesType.java
@@ -3,97 +3,54 @@ package app;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
-/**
- * Example Index HTML class using Javalin
- * <p>
- * Generate a static HTML page using Javalin
- * by writing the raw HTML into a Java String object
- *
- * @author Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
- * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
- */
 public class PageMoviesType implements Handler {
 
-    // URL of this page relative to http://localhost:7001/
     public static final String URL = "/moviestype.html";
 
-    // Name of the Thymeleaf HTML template page in the resources folder
-    private static final String TEMPLATE = ("moviestype.html");
+    private static final String TEMPLATE = "moviestype.html";
 
     @Override
     public void handle(Context context) throws Exception {
-        // The model of data to provide to Thymeleaf.
-        // In this example the model will be filled with:
-        //  - Title to give to the h1 tag
-        //  - Array list of all movies for the UL element
-        Map<String, Object> model = new HashMap<String, Object>();
+        Map<String, Object> model = new HashMap<>();
+        model.put("title", "Movies by Type");
 
-        // Add in title for the h1 tag to the model
-        model.put("title", new String("Movies by Type"));
-
-        // Add into the model the list of types to give to the dropdown
-        ArrayList<String> types = new ArrayList<String>();
+        ArrayList<String> types = new ArrayList<>();
         types.add("HORROR");
         model.put("types", types);
 
-        // Look up from JDBC
         JDBCConnection jdbc = new JDBCConnection();
 
-        /* Get the Form Data
-         *  from the drop down list
-         * Need to be Careful!!
-         *  If the form is not filled in, then the form will return null!
-        */
         String movietype_drop = context.formParam("movietype_drop");
         if (movietype_drop == null) {
-            // If NULL, nothing to show, therefore we make some "no results" HTML
-            // Also store empty array list for completness
-            model.put("title_drop", new String("No Results to show for dropbox"));
-            ArrayList<String> movies = new ArrayList<String>();
-            model.put("movies_drop", movies);
+            model.put("title_drop", "No Results to show for dropdown");
+            model.put("movies_drop", new ArrayList<String>());
         } else {
-            // If NOT NULL, then lookup the movie by type!
-            model.put("title_drop", new String(movietype_drop + " Movies"));
+            model.put("title_drop", movietype_drop + " Movies");
             ArrayList<Movie> movies = jdbc.getMoviesByType(movietype_drop);
-            ArrayList<String> titles = extractMovieTitles(movies);
-            model.put("movies_drop", titles);
+            model.put("movies_drop", extractMovieTitles(movies));
         }
 
         String movietype_textbox = context.formParam("movietype_textbox");
-        if (movietype_textbox == null || movietype_textbox == "") {
-            // If NULL, nothing to show, therefore we make some "no results" HTML
-            // Also store empty array list for completness
-            model.put("title_text", new String("No Results to show for textbox"));
-            ArrayList<String> movies = new ArrayList<String>();
-            model.put("movies_text", movies);
+        if (movietype_textbox == null || movietype_textbox.isEmpty()) {
+            model.put("title_text", "No Results to show for textbox");
+            model.put("movies_text", new ArrayList<String>());
         } else {
-            // If NOT NULL, then lookup the movie by type!
-            model.put("title_text", new String(movietype_textbox + " Movies"));
+            model.put("title_text", movietype_textbox + " Movies");
             ArrayList<Movie> movies = jdbc.getMoviesByType(movietype_textbox);
-            ArrayList<String> titles = extractMovieTitles(movies);
-            model.put("movies_text", titles);
+            model.put("movies_text", extractMovieTitles(movies));
         }
 
-        // DO NOT MODIFY THIS
-        // Makes Javalin render the webpage using Thymeleaf
         context.render(TEMPLATE, model);
     }
 
-    /**
-     * This extracts the ArrayList of Movie Titles from the provided
-     * array list of movies. This is needed to pass an arraylist of
-     * strings to Thymeleaf as we can't use our own custom classes.
-     */
-    ArrayList<String> extractMovieTitles(ArrayList<Movie> movies) {
-        ArrayList<String> titles = new ArrayList<String>();
+    private ArrayList<String> extractMovieTitles(ArrayList<Movie> movies) {
+        ArrayList<String> titles = new ArrayList<>();
         for (Movie movie : movies) {
             titles.add(movie.name);
         }
         return titles;
     }
-
 }
